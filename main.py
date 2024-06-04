619a72566054df2ae46bece45c5b8343099d634c
livy <aishenliwei@163.com>
2024-06-04 11:53:29 +0800

    打开图片自适应

--- main.py
+++ main.py
@@ -1,47 +1,52 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QAction, QToolBar, QMainWindow, QFileDialog
+import os
+import json
+import datetime
+from decimal import Decimal, getcontext
+
+getcontext().prec = 10
+getcontext().rounding = 'ROUND_HALF_UP'
+
+from tkinter import Tk
+
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QAction, QToolBar, QMainWindow, QFileDialog, \
+    QMessageBox
 from PyQt5.QtGui import QPixmap
 
-class ImageDisplay(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
+from Root_Preprocess import Ui_MainWindow
 
-    def initUI(self):
-        self.setWindowTitle('Image Display')
-        self.setGeometry(100, 100, 400, 200)
 
-        self.toolbar = QToolBar()
-        self.addToolBar(self.toolbar)
+class UiMain(QMainWindow, Ui_MainWindow):
 
-        open_action = QAction('Open', self)
-        open_action.triggered.connect(self.openImage)
-        self.toolbar.addAction(open_action)
+    def __init__(self, parent=None):
+        super(UiMain, self).__init__(parent)
+        self.info = 'info.json'
+        self.setupUi(self)
+        self.bing_signal()
+        self.init_style()
 
-        exit_action = QAction('Exit', self)
-        exit_action.triggered.connect(self.close)
-        self.toolbar.addAction(exit_action)
+    def bing_signal(self):
+        self.open_img_btn.clicked.connect(self.openImage)
+        pass
 
-        layout = QVBoxLayout()
+    def init_style(self):
+        pass
 
-        self.label1 = QLabel("放大")
-        layout.addWidget(self.label1)
-
-        self.label2 = QLabel("缩小")
-        layout.addWidget(self.label2)
-
-        self.setLayout(layout)
 
     def openImage(self):
         options = QFileDialog.Options()
-        file_path, _ = QFileDialog.getOpenFileName(self, "Open Image", "", "Image Files (*.png *.jpg *.jpeg *.bmp *.gif)", options=options)
+        file_path, _ = QFileDialog.getOpenFileName(self, "Open Image", "",
+                                                   "Image Files (*.png *.jpg *.jpeg *.bmp *.gif)", options=options)
         if file_path:
             pixmap = QPixmap(file_path)
-            self.label1.setPixmap(pixmap)
-            self.label2.setPixmap(pixmap)
+            self.image_label_1.setPixmap(pixmap)
+            self.image_label_1.setScaledContents(True)
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = ImageDisplay()
-    window.show()
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    win = UiMain()
+    win.show()
     sys.exit(app.exec_())
