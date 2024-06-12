09b1d7c40e10e311a83cbcd46f28bd8d402a323d
liuqimin <liuqimin2009@163.com>
2024-06-12 16:44:24 +0800

    test

--- main1.py
+++ main1.py
@@ -13,8 +13,9 @@ from PyQt5.QtGui import QImage
 import cv2
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QAction, QToolBar, QMainWindow, QFileDialog, \
-    QMessageBox
+    QMessageBox, QDesktopWidget
 from PyQt5.QtGui import QPixmap, qGray
+from PyQt5.QtCore import Qt
 
 from PIL import Image
 
@@ -23,15 +24,32 @@ from Root_Preprocess import Ui_MainWindow
 
 class UiMain(QMainWindow, Ui_MainWindow):
 
+    def setWindow(self):
+        screen = QDesktopWidget().screenGeometry()
+        window_width = screen.width() 
+        window_height = screen.height() 
+        self.setGeometry(screen.center().x() - window_width // 2, screen.center().y() - window_height // 2, window_width, window_height)
+
     def __init__(self, parent=None):
         super(UiMain, self).__init__(parent)
         self.info = 'info.json'
         self.setupUi(self)
+
+
         self.bing_signal()
         self.init_style()
         self.threshold=0
         self.pixmap = QPixmap()
         self.gray_pixmap = QPixmap()
+        self.q_image=None
+
+    def resize_event(self, event):
+        print('resize event:', self.size())
+        print('resize event:', self.image_label_1.size())
+        self.image_label_1.resize(self.size())
+        if self.q_image:
+            self.update_image(self.image_label_2, self.pixmap)
+            self.update_image(self.image_label_1, self.gray_pixmap)
 
     def bing_signal(self):
         self.open_img_btn.clicked.connect(self.openImage)
@@ -39,6 +57,7 @@ class UiMain(QMainWindow, Ui_MainWindow):
         self.plus_btn.clicked.connect(self.plus_button_clicked)
         self.minus_btn.clicked.connect(self.minus_button_clicked)
         self.threshold_apply_btn.clicked.connect(self.apply_threshold)  
+        self.resizeEvent=self.resize_event
         pass
 
     def init_style(self):
@@ -90,11 +109,12 @@ class UiMain(QMainWindow, Ui_MainWindow):
         return cv2.cvtColor(arr, cv2.COLOR_RGBA2BGR)
 
 
-    def update_image(self, gray_image):
-        #scaledPixmap = self.pixmap.scaled(self.size(), Qt.KeepAspectRatio)
-        self.gray_pixmap = QPixmap.fromImage(gray_image)
-        self.image_label_1.setPixmap(self.gray_pixmap)
-        self.image_label_1.setScaledContents(True)
+    def update_image(self, image_label, pixmap):
+        scaledPixmap = pixmap.scaled(self.image_label_1.size(), Qt.KeepAspectRatio)
+        print(self.size(), 'label size:')
+
+        image_label.setPixmap(scaledPixmap)
+        image_label.setScaledContents(True)
 
     def openImage(self):
         options = QFileDialog.Options()
@@ -102,15 +122,16 @@ class UiMain(QMainWindow, Ui_MainWindow):
                                                    "Image Files (*.png *.jpg *.jpeg *.bmp *.gif)", options=options)
 
         if file_path:
-            self.pil_image=Image.open(file_path)
-            self.pixmap = QPixmap(file_path)
+            #self.pil_image=Image.open(file_path)
+            self.q_image=QImage(file_path)
+            gray_image = self.q_image.convertToFormat(QImage.Format_Grayscale8)
+            self.pixmap = QPixmap.fromImage(self.q_image)            
+            self.gray_pixmap=QPixmap.fromImage(gray_image)
+
             self.image_label_2.setPixmap(self.pixmap)
             self.image_label_2.setScaledContents(True)
-
-            self.q_image = self.pixmap.toImage()
-            gray_image = self.q_image.convertToFormat(QImage.Format_Grayscale8)
       
-            self.update_image(gray_image)
+            self.update_image(self.image_label_1, self.gray_pixmap)
 
 
 
